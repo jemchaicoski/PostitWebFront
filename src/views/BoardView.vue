6f3229f39731eb1e90d1efbf0263d8117a0e7440
Jemchaicoski <jemchaicoski@hotmail.com>
2024-05-05 15:52:27 -0300

    add: routes ands views

---
+++ src/views/BoardView.vue
@@ -0,0 +1,8 @@
+<template>
+    <main class="board-page">
+        <h1>Board</h1>
+        <p>Tela do Board</p>
+    </main>
+</template>
+
+<style></style>
---
+++ src/views/UserView.vue
@@ -0,0 +1,8 @@
+<template>
+    <main class="user-page">
+        <h1>User</h1>
+        <p>Tela do User</p>
+    </main>
+</template>
+
+<style></style>
