d6047e53d248f8216fd0dc83688c286a27c1c6dd
Jemchaicoski <jemchaicoski@hotmail.com>
2024-05-05 18:38:20 -0300

    Sidebar wip

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+    <aside>
+        Sidebar
+    </aside>
+</template>
+
+<script setup>
+
+</script>
+
+<style Lang="sass" scoped>
+aside {
+    display: flex;
+    flex-direction: column;
+
+    background-color: var(--vt-c-black-soft);
+    color: var(--vt-c-white-soft)
+}
+</style>
